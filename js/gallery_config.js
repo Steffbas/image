bfd5b2d81710db1e16bfa0da180a28dd39f6edde
GitHub Action <action@github.com>
2026-02-17 18:17:14 +0000

    Auto-update portfolio data [skip ci]

--- js/gallery_config.js
+++ js/gallery_config.js
@@ -1,5 +1,11 @@
-const GALLERY_VERSION = 1771352079;
+const GALLERY_VERSION = 1771352234;
 const GALLERY_ITEMS = [
+  {
+    "id": "DreamShaper_v5_cute_tiny_hyperrealistic_Anime_rabbit_from_pok_1.jpg",
+    "category": "mariages",
+    "src": "images/mariages/DreamShaper_v5_cute_tiny_hyperrealistic_Anime_rabbit_from_pok_1.jpg",
+    "title": "Dreamshaper V5 Cute Tiny Hyperrealistic Anime Rabbit From Pok 1"
+  },
   {
     "id": "about-me.jpg",
     "category": "reportages",
