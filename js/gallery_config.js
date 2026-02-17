77f76b69d2614b5abcfee896bbd108dfea6efaf7
GitHub Action <action@github.com>
2026-02-17 18:14:39 +0000

    Auto-update portfolio data [skip ci]

--- js/gallery_config.js
+++ js/gallery_config.js
@@ -1,2 +1,9 @@
-const GALLERY_VERSION = 1771351834;
-const GALLERY_ITEMS = [];
+const GALLERY_VERSION = 1771352079;
+const GALLERY_ITEMS = [
+  {
+    "id": "about-me.jpg",
+    "category": "reportages",
+    "src": "images/reportages/about-me.jpg",
+    "title": "About Me"
+  }
+];
